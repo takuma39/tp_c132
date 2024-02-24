11afd4f58bad39037dc83e3fb4ab00ca6792d7fd
takuma39 <takuma.sato.job@gmail.com>
2024-02-25 02:34:43 +0900

    ADD-0003：add：routes修正

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ログイン画面
+Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
+// ユーザー登録画面
+Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
+// メモ一覧画面
+Route::get('/memo', function () {
+    return view("memo");
+})->name('memo.index');
 
 Auth::routes();
 
