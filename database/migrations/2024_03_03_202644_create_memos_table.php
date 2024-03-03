8893b925a6d6d8b75062d6cf193f428f045adef1
takuma39 <takuma.sato.job@gmail.com>
2024-03-03 20:32:11 +0900

    ADD-0005：fix：migration修正

--- database/migrations/2024_03_03_202644_create_memos_table.php
+++ database/migrations/2024_03_03_202644_create_memos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('memos', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
