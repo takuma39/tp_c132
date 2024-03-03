62f06b5cfa73a16cc327259b0ab715dfba590d74
takuma39 <takuma.sato.job@gmail.com>
2024-03-03 19:26:37 +0900

    ADD-0004：add：middleware追加

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name
 Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
 Route::post('/user/register', 'App\Http\Controllers\Auth\RegisterController@register')->name('user.exec.register');
 // メモ一覧画面
-Route::get('/memo', function () {
-    return view("memo");
-})->name('memo.index');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/memo', function () {
+        return view("memo");
+    })->name('memo.index');
+});
 
 Auth::routes();
 
